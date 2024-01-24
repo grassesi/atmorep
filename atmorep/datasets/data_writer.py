11a9ff24d2df98cde793bdb1124101152f46cf51
Simon Grasse <s.grasse@fz-juelich.de>
2024-01-24 21:56:44 +0100

    refactor data_writer

--- atmorep/datasets/data_writer.py
+++ atmorep/datasets/data_writer.py
@@ -14,178 +14,121 @@
 #
 ####################################################################################################
 
-import numpy as np
-import xarray as xr
 import zarr
-import code
-import datetime
+
 import atmorep.config.config as config
 
-####################################################################################################
-def write_forecast( model_id, epoch, batch_idx, levels, sources, sources_coords,
-                                                targets, targets_coords,
-                                                preds, ensembles,
-                                                zarr_store_type = 'ZipStore' ) :
+def write_batch_item_forecast(
+  batch_item, fidx, bidx, data, levels, coords
+):
+  time, lat, lon = coords
+  batch_item.create_dataset( 'data', data=data[bidx])
+  batch_item.create_dataset( 'ml', data=levels)
+  batch_item.create_dataset( 'datetime', data=time[bidx])
+  batch_item.create_dataset( 'lat', data=lat[bidx])
+  batch_item.create_dataset( 'lon', data=lon[bidx])
+
+def write_batch_item_BERT(
+  batch_item, fidx, bidx, data, levels, coords
+):
+  time, lat, lon = coords[:3]
+  batch_item.create_dataset( 'data', data=data[bidx])
+  batch_item.create_dataset( 'ml', data=levels[fidx])
+  batch_item.create_dataset( 'datetime', data=time[0][bidx])
+  batch_item.create_dataset( 'lat', data=lat[0][bidx])
+  batch_item.create_dataset( 'lon', data=lon[0][bidx])
+
+def write_batch_item_BERT_vertical(
+  batch_item, fidx, bidx, data, levels, coords
+):
+  time, lat, lon = coords[:3]
+  for vidx in range(len(levels[fidx])) :
+    m_lvl = batch_item.require_group( f'ml={levels[fidx][vidx]}')
+    m_lvl.create_dataset( 'data', data=data[vidx][bidx])
+    m_lvl.create_dataset( 'ml', data=levels[fidx][vidx])
+    m_lvl.create_dataset( 'datetime', data=time[fidx][bidx][vidx])
+    m_lvl.create_dataset( 'lat', data=lat[fidx][bidx][vidx])
+    m_lvl.create_dataset( 'lon', data=lon[fidx][bidx][vidx])
+
+def get_zarr_store(name, model_id, epoch, zarr_store_type):
+  fname =  f'{config.path_results}/id{model_id}/results_id{model_id}_epoch{epoch:05d}' + '_{}.zarr'
+  return zarr_store_type( fname.format(name))
+
+def write_zarr(
+  name, fields, model_id, epoch, zarr_store_type, batch_idx, levels, coords, batch_item_write
+):
+  store = get_zarr_store(name, model_id, epoch, zarr_store_type)
+  zarr_group = zarr.group(store=store)
+  for fidx, field in enumerate(fields):
+    name, data = field
+    
+    # for BERT: skip fields that were not predicted
+    if (
+      name in ["target", "pred", "ens"] and
+      batch_item_write == write_batch_item_BERT_vertical and
+      len(data[0]) == 0
+    ):
+      continue
+      
+    var_subgroup = zarr_group.require_group( f'{name}')
+    batch_size = data.shape[0]
+    for bidx in range(batch_size):
+      sample_id = batch_idx * batch_size + bidx
+      batch_item_subgroup = var_subgroup.create_group(f"sample={sample_id:05d}")
+      batch_item_write(
+        batch_item_subgroup, fidx, bidx, data, levels, coords
+      )
+      
+  store.close()
+
+def write_forecast(
+  model_id, epoch, batch_idx, levels, sources, sources_coords, targets, targets_coords, preds, ensembles, zarr_store_type_str='ZipStore'
+):
   ''' 
     sources : num_fields x [field name , data]
     targets :
     preds, ensemble share coords with targets
   '''
+  zarr_store_type = getattr( zarr, zarr_store_type_str)
+  
+  write_zarr(
+    "source", sources, model_id, epoch, zarr_store_type, batch_idx, levels, sources_coords, write_batch_item_forecast
+  )
+  write_zarr(
+    "target", targets, model_id, epoch, zarr_store_type, batch_idx, levels, targets_coords, write_batch_item_forecast
+  )
+  write_zarr(
+    "pred", preds, model_id, epoch, zarr_store_type, batch_idx, levels, targets_coords, write_batch_item_forecast
+  )
+  write_zarr(
+    "ens", ensembles, model_id, epoch, zarr_store_type, batch_idx, levels, targets_coords, write_batch_item_forecast
+  )
 
-  fname =  f'{config.path_results}/id{model_id}/results_id{model_id}_epoch{epoch:05d}' + '_{}.zarr'
 
-  zarr_store = getattr( zarr, zarr_store_type)
-
-  store_source = zarr_store( fname.format( 'source'))
-  exp_source = zarr.group(store=store_source)
-  for fidx, field in enumerate(sources) :
-    ds_field = exp_source.require_group( f'{field[0]}')
-    batch_size = field[1].shape[0]
-    for bidx in range( field[1].shape[0]) :
-      sample = batch_idx * batch_size + bidx
-      ds_batch_item = ds_field.create_group( f'sample={sample:05d}' )
-      ds_batch_item.create_dataset( 'data', data=field[1][bidx])
-      ds_batch_item.create_dataset( 'ml', data=levels)
-      ds_batch_item.create_dataset( 'datetime', data=sources_coords[0][bidx])
-      ds_batch_item.create_dataset( 'lat', data=sources_coords[1][bidx])
-      ds_batch_item.create_dataset( 'lon', data=sources_coords[2][bidx])
-  store_source.close()
-
-  store_target = zarr_store( fname.format( 'target'))
-  exp_target = zarr.group(store=store_target)
-  for fidx, field in enumerate(targets) :
-    ds_field = exp_target.require_group( f'{field[0]}')
-    batch_size = field[1].shape[0]
-    for bidx in range( field[1].shape[0]) :
-      sample = batch_idx * batch_size + bidx
-      ds_batch_item = ds_field.create_group( f'sample={sample:05d}' )
-      ds_batch_item.create_dataset( 'data', data=field[1][bidx])
-      ds_batch_item.create_dataset( 'ml', data=levels)
-      ds_batch_item.create_dataset( 'datetime', data=targets_coords[0][bidx])
-      ds_batch_item.create_dataset( 'lat', data=targets_coords[1][bidx])
-      ds_batch_item.create_dataset( 'lon', data=targets_coords[2][bidx])
-  store_target.close()
-
-  store_pred = zarr_store( fname.format( 'pred'))
-  exp_pred = zarr.group(store=store_pred)
-  for fidx, field in enumerate(preds) :
-    ds_field = exp_pred.require_group( f'{field[0]}')
-    batch_size = field[1].shape[0]
-    for bidx in range( field[1].shape[0]) :
-      sample = batch_idx * batch_size + bidx
-      ds_batch_item = ds_field.create_group( f'sample={sample:05d}' )
-      ds_batch_item.create_dataset( 'data', data=field[1][bidx])
-      ds_batch_item.create_dataset( 'ml', data=levels)
-      ds_batch_item.create_dataset( 'datetime', data=targets_coords[0][bidx])
-      ds_batch_item.create_dataset( 'lat', data=targets_coords[1][bidx])
-      ds_batch_item.create_dataset( 'lon', data=targets_coords[2][bidx])
-  store_pred.close()
-
-  store_ens = zarr_store( fname.format( 'ens'))
-  exp_ens = zarr.group(store=store_ens)
-  for fidx, field in enumerate(ensembles) :
-    ds_field = exp_ens.require_group( f'{field[0]}')
-    batch_size = field[1].shape[0]
-    for bidx in range( field[1].shape[0]) :
-      sample = batch_idx * batch_size + bidx
-      ds_batch_item = ds_field.create_group( f'sample={sample:05d}' )
-      ds_batch_item.create_dataset( 'data', data=field[1][bidx])
-      ds_batch_item.create_dataset( 'ml', data=levels)
-      ds_batch_item.create_dataset( 'datetime', data=targets_coords[0][bidx])
-      ds_batch_item.create_dataset( 'lat', data=targets_coords[1][bidx])
-      ds_batch_item.create_dataset( 'lon', data=targets_coords[2][bidx])
-  store_ens.close()
-
-####################################################################################################
-def write_BERT( model_id, epoch, batch_idx, levels, sources, sources_coords,
-                                            targets, targets_coords,
-                                            preds, ensembles,
-                                            zarr_store_type = 'ZipStore' ) :
+def write_BERT(
+  model_id, epoch, batch_idx, levels, sources, sources_coords, targets, targets_coords, preds, ensembles, zarr_store_type_str='ZipStore'
+) :
   '''
     sources : num_fields x [field name , data]
     targets :
     preds, ensemble share coords with targets
   '''
+  zarr_store_type = getattr( zarr, zarr_store_type_str)
+  
+  write_zarr(
+    "source", sources, model_id, epoch, zarr_store_type, batch_idx, levels, sources_coords, write_batch_item_BERT
+  )
+  write_zarr(
+    "target", targets, model_id, epoch, zarr_store_type, batch_idx, levels, targets_coords, write_batch_item_BERT_vertical
+  )
+  write_zarr(
+    "pred", preds, model_id, epoch, zarr_store_type, batch_idx, levels, targets_coords, write_batch_item_BERT_vertical
+  )
+  write_zarr(
+    "ens", ensembles, model_id, epoch, zarr_store_type, batch_idx, levels, targets_coords, write_batch_item_BERT_vertical
+  )
 
-  # fname =  f'{config.path_results}/id{model_id}/results_id{model_id}_epoch{epoch}.zarr'
-  fname =  f'{config.path_results}/id{model_id}/results_id{model_id}_epoch{epoch:05d}' + '_{}.zarr'
 
-  zarr_store = getattr( zarr, zarr_store_type)
-
-  store_source = zarr_store( fname.format( 'source'))
-  exp_source = zarr.group(store=store_source)
-  for fidx, field in enumerate(sources) :
-    ds_field = exp_source.require_group( f'{field[0]}')
-    batch_size = field[1].shape[0]
-    for bidx in range( field[1].shape[0]) :
-      sample = batch_idx * batch_size + bidx
-      ds_batch_item = ds_field.create_group( f'sample={sample:05d}' )
-      ds_batch_item.create_dataset( 'data', data=field[1][bidx])
-      ds_batch_item.create_dataset( 'ml', data=levels[fidx])
-      ds_batch_item.create_dataset( 'datetime', data=sources_coords[0][0][bidx])
-      ds_batch_item.create_dataset( 'lat', data=sources_coords[1][0][bidx])
-      ds_batch_item.create_dataset( 'lon', data=sources_coords[2][0][bidx])
-  store_source.close()
-
-  store_target = zarr_store( fname.format( 'target'))
-  exp_target = zarr.group(store=store_target)
-  for fidx, field in enumerate(targets) :
-    if 0 == len(field[1]) :  # skip fields that were not predicted
-        continue
-    batch_size = len(field[1][0])
-    ds_field = exp_target.require_group( f'{field[0]}')
-    for bidx in range( len(field[1][0])) :
-      sample = batch_idx * batch_size + bidx
-      ds_target_b = ds_field.create_group( f'sample={sample:05d}')
-      for vidx in range(len(levels[fidx])) :
-        ds_target_b_l = ds_target_b.require_group( f'ml={levels[fidx][vidx]}')
-        ds_target_b_l.create_dataset( 'data', data=field[1][vidx][bidx])
-        ds_target_b_l.create_dataset( 'ml', data=levels[fidx][vidx])
-        ds_target_b_l.create_dataset( 'datetime', data=targets_coords[0][fidx][bidx][vidx])
-        ds_target_b_l.create_dataset( 'lat', data=targets_coords[1][fidx][bidx][vidx])
-        ds_target_b_l.create_dataset( 'lon', data=targets_coords[2][fidx][bidx][vidx])
-  store_target.close()
-
-  store_pred = zarr_store( fname.format( 'pred'))
-  exp_pred = zarr.group(store=store_pred)
-  for fidx, field in enumerate(preds) :
-    if 0 == len(field[1]) :  # skip fields that were not predicted
-      continue
-    batch_size = len(field[1][0])
-    ds_pred = exp_pred.require_group( f'{field[0]}')
-    for bidx in range( len(field[1][0])) :
-      sample = batch_idx * batch_size + bidx
-      ds_pred_b = ds_pred.create_group( f'sample={sample:05d}')
-      for vidx in range(len(levels[fidx])) :
-        ds_pred_b_l = ds_pred_b.create_group( f'ml={levels[fidx][vidx]}')
-        ds_pred_b_l.create_dataset( 'data', data
-                                   =field[1][vidx][bidx])
-        ds_pred_b_l.create_dataset( 'ml', data=levels[fidx][vidx])
-        ds_pred_b_l.create_dataset( 'datetime', data=targets_coords[0][fidx][bidx][vidx])
-        ds_pred_b_l.create_dataset( 'lat', data=targets_coords[1][fidx][bidx][vidx])
-        ds_pred_b_l.create_dataset( 'lon', data=targets_coords[2][fidx][bidx][vidx])
-  store_pred.close()
-
-  store_ens = zarr_store( fname.format( 'ens'))
-  exp_ens = zarr.group(store=store_ens)
-  for fidx, field in enumerate(ensembles) :
-    if 0 == len(field[1]) :  # skip fields that were not predicted
-      continue
-    batch_size = len(field[1][0])
-    ds_ens = exp_ens.require_group( f'{field[0]}')
-    for bidx in range( len(field[1][0])) :
-      sample = batch_idx * batch_size + bidx
-      ds_ens_b = ds_ens.create_group( f'sample={sample:05d}')
-      for vidx in range(len(levels[fidx])) :
-        ds_ens_b_l = ds_ens_b.create_group( f'ml={levels[fidx][vidx]}')
-        ds_ens_b_l.create_dataset( 'data', data=field[1][vidx][bidx])
-        ds_ens_b_l.create_dataset( 'ml', data=levels[fidx][vidx])
-        ds_ens_b_l.create_dataset( 'datetime', data=targets_coords[0][fidx][bidx][vidx])
-        ds_ens_b_l.create_dataset( 'lat', data=targets_coords[1][fidx][bidx][vidx])
-        ds_ens_b_l.create_dataset( 'lon', data=targets_coords[2][fidx][bidx][vidx])
-  store_ens.close()
-
-####################################################################################################
 def write_attention(model_id, epoch, batch_idx, levels, attn, attn_coords, zarr_store_type = 'ZipStore' ) :
 
   fname =  f'{config.path_results}/id{model_id}/results_id{model_id}_epoch{epoch:05d}' + '_{}.zarr'
