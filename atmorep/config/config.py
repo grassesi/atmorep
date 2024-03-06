84e5e4308c85fccaa1aa4154943f940428a9858b
Simon Grasse <s.grasse@fz-juelich.de>
2024-03-06 10:44:03 +0100

    use pathlib instead of os.path

--- atmorep/config/config.py
+++ atmorep/config/config.py
@@ -1,12 +1,11 @@
-import os 
 from pathlib import Path
 
-FPATH = os.path.dirname(os.path.realpath(__file__))
+ATMOREP_REPO_ROOT = Path(__file__).parent.parent.parent
 
 YEAR_BASE = 1979
 YEAR_LAST = 2022
 
-PATH_MODELS = Path( FPATH, '../../models/')
-PATH_RESULTS = Path( FPATH, '../../results/')
-PATH_DATA = Path( FPATH, '../../data/')
+PATH_MODELS = ATMOREP_REPO_ROOT / 'models/'
+PATH_RESULTS = ATMOREP_REPO_ROOT / 'results/'
+PATH_DATA = ATMOREP_REPO_ROOT / 'data/'
 
