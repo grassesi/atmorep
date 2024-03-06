7f9cf9d4f16416958bfbf44acee19b3706c35c5b
Simon Grasse <s.grasse@fz-juelich.de>
2024-03-06 10:43:07 +0100

    remove dead code

--- atmorep/config/config.py
+++ atmorep/config/config.py
@@ -9,11 +9,4 @@ year_last = 2022
 path_models = Path( fpath, '../../models/')
 path_results = Path( fpath, '../../results/')
 path_data = Path( fpath, '../../data/')
-path_plots = Path( fpath, '../results/plots/')
 
-grib_index = { 'vorticity' : 'vo', 'divergence' : 'd', 'geopotential' : 'z',
-                'orography' : 'z', 'temperature': 't', 'specific_humidity' : 'q',
-                'mean_top_net_long_wave_radiation_flux' : 'mtnlwrf',
-                'velocity_u' : 'u', 'velocity_v': 'v', 'velocity_z' : 'w',
-                'total_precip' : 'tp', 'radar_precip' : 'yw_hourly',
-                't2m' : 't_2m', 'u_10m' : 'u_10m', 'v_10m' : 'v_10m',  }
